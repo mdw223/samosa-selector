893d713795fd2210d8d1cc5cb3a15cea76bdc82e
Malik <malikadidw@gmail.com>
2025-08-14 10:43:49 -0400

    deployment using github actions

--- vite.config.js
+++ vite.config.js
@@ -4,16 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/samosa-selector',
-  build: {
-    rollupOptions: {
-      output: {
-        assetFileNames: 'assets/[name]-[hash][extname]',
-        chunkFileNames: 'assets/[name]-[hash].js',
-        entryFileNames: 'assets/[name]-[hash].js',
-      },
-    },
-    // Force all assets to be emitted as files (not inlined)
-    assetsInlineLimit: 0,
-  },
+  base: '/samosa-selector/',
 })
