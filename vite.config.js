d7db405c7eccc0b246549a070c64f932cda1940e
Malik <malikadidw@gmail.com>
2025-08-14 10:14:10 -0400

    config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/samosa-selector/',
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name]-[hash][extname]',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js',
+      },
+    },
+    // Force all assets to be emitted as files (not inlined)
+    assetsInlineLimit: 0,
+  },
 })
